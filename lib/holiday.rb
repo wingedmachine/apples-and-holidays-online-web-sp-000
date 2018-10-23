1930ac2666e8d0c510edba0cf1daa4ed3510f9ad
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-23 01:44:02 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -55,7 +55,11 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_hash.each do |season|
+    season.each do |holiday|
+      binding.pry
+    end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
