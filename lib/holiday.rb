7fe5382c2454f02431a52d91889e0567813ba25c
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-23 02:04:33 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holidays|
     puts titleize_symbol(season) + ":"
     holidays.each do |holiday, supplies|
-
+      binding.pry
     end
   end
 end
